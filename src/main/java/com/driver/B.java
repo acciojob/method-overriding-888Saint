2641137b061915ff6ba8d04eff2a77fb4cb2b7c9
888Saint <psdas32@gmail.com>
2024-10-16 19:44:23 +0530

    Delete src/main/java/com/driver/B.java

--- src/main/java/com/driver/B.java
+++
@@ -1,9 +0,0 @@
-package com.driver;
-
-public class B extends A {
-    // Overriding the meth() method from class A
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
