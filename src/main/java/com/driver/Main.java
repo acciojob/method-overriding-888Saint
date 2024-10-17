f6d45c48ee114486376a8b50b31732f929c1bb50
888Saint <psdas32@gmail.com>
2024-10-17 12:41:10 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,6 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        // Create an object of class B
-        B obj = new B();
-        B obj1 = new B();
-        // Call the method from class A using class B object
-        //System.out.println(obj.meth());  // This will call the method from class A
-    }
-
     public static class A {
     // Method in class A
     public String meth() {
@@ -16,13 +8,24 @@ public class Main {
     }
 }
 
-public static class B extends A {
+public static class B extends A
+    {
     // Overriding the meth() method from class A
     @Override
     public String meth() {
         return "Method is overridden in Extended class B";
     }
-}
+   }
+
+ public static void main(String[] args) {
+        // Create an object of class B
+        B obj = new B();
+        obj.meth();
+        B obj1 = new B();
+        obj1.meth();
+        // Call the method from class A using class B object
+        //System.out.println(obj.meth());  // This will call the method from class A
+    }
 }
 
 
